fedb9849039fac6d6c93ce36361aa7060052bd3c
kbrajeshwaran <kbrajeshwaran@gmail.com>
2022-12-27 00:43:54 +0400

    updated build status image & acr.sh script

---
+++ acr.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ACR_NAME="devopsforu3013"
+RES_GROUP="rg-devopsforu3013"
+LOC="eastus"
+
+# Create Resource Group
+az group create -n $RES_GROUP -l $LOC
+
+# Create ACR 
+az acr create --resource-group $RES_GROUP --name $ACR_NAME --sku basic --location $LOC
+
+# Delete Resource_Group
+
+# az group delete -n $RES_GROUP 
+
+# Delete ACR
+
+# az acr delete -n $ACR_NAME
