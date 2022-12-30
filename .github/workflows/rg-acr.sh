baa8c898d982e3c349b215c38ed705d63b1b9b61
Rajeshwaran <112589969+rajeshwarandevops@users.noreply.github.com>
2022-12-30 10:47:19 +0400

    Update rg-acr.sh

--- .github/workflows/rg-acr.sh
+++ .github/workflows/rg-acr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-chmod +x $GITHUB_WORKSPACE/rg_acr.sh
 
 #Variables
 
@@ -10,7 +9,7 @@ RES_GROUP="rg-devopsforu3013" ( unique)
 
 LOCATION="eastus" ( based on your preferred location)
 
-# the below script will create Resource group & ACR in Azure cloud based on Variabled defined on #Variables secion
-az group create -n $RES_GROUP -l $LOC
-az acr create --resource-group $RES_GROUP --name $ACR_NAME --sku basic --location $LOATION
+# below script will create Resource group & ACR in Azure cloud based on Variables defined on #Variables section
+az group create -n $RES_GROUP -l $LOCATION
+az acr create --resource-group $RES_GROUP --name $ACR_NAME --sku basic --location $LOCATION
 
